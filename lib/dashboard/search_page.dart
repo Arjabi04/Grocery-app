600100a06b3f358dd7545c8963d1c6a83219c1ec
Arjabi04 <arjabishrestha@gmail.com>
2025-08-23 17:45:22 +0545

    complete search page created

--- lib/dashboard/search_page.dart
+++ lib/dashboard/search_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/widget/category_selector.dart';
 import 'package:grocery_app/widget/top_navbar.dart';
+import 'package:grocery_app/widget/searchbar.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -24,9 +26,127 @@ class SearchPage extends StatelessWidget {
                 ],
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SearchBarWidget(hintText: "Search anything"),
+                  const SizedBox(height: 20),
+                  IconCategorySelector(
+                    categories: [
+                      IconCategoryItem(
+                        label: "Filter",
+                        icon: Icons.filter_list,
+                      ),
+                      IconCategoryItem(label: "Fruits"),
+                      IconCategoryItem(label: "Vegetables"),
+                      IconCategoryItem(label: "Dairy"),
+                      IconCategoryItem(label: "Bakery"),
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TrendingItems(
+                        title: 'Trending Items',
+                        items: [
+                          'Vegan-Friendly Milk',
+                          'Zero Sugar Soda',
+                          'Fructose-Free Syrup',
+                        ],
+                        icons: List.generate(
+                          3,
+                          (_) => const Icon(
+                            Icons.search,
+                            size: 18,
+                          ), // same search icon for all
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TrendingItems(
+                        title: 'Recent Searches',
+                        items: [
+                          'Chicken Breast',
+                          'Japanese Kobe Beef',
+                          'Salted Egg',
+                        ],
+                        icons: List.generate(
+                          3,
+                          (_) => const Icon(
+                            Icons.history,
+                            size: 18,
+                          ), // same history icon for all
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class TrendingItems extends StatelessWidget {
+  final String title;
+  final List<String> items;
+  final List<Widget>? icons;
+
+  const TrendingItems({
+    super.key,
+    required this.title,
+    required this.items,
+    this.icons,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            color: Color(0xFF0D0D0D),
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const SizedBox(height: 12),
+        ...List.generate(items.length, (index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: Row(
+              children: [
+                icons != null && index < icons!.length
+                    ? icons![index]
+                    : Container(
+                        width: 18,
+                        height: 18,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[400],
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                const SizedBox(width: 12),
+                Text(
+                  items[index],
+                  style: const TextStyle(
+                    color: Color(0xFF777777),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
+      ],
     );
   }
 }
--- lib/widget/category_selector.dart
+++ lib/widget/category_selector.dart
@@ -55,3 +55,80 @@ class _CategorySelectorState extends State<CategorySelector> {
     );
   }
 }
+
+class IconCategoryItem {
+  final String label;
+  final IconData? icon;
+
+  IconCategoryItem({required this.label, this.icon});
+}
+
+class IconCategorySelector extends StatefulWidget {
+  final List<IconCategoryItem> categories;
+
+  const IconCategorySelector({super.key, required this.categories});
+
+  @override
+  State<IconCategorySelector> createState() => _IconCategorySelectorState();
+}
+
+class _IconCategorySelectorState extends State<IconCategorySelector> {
+  int? selectedIndex; // initially no selection
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: widget.categories.length,
+        separatorBuilder: (context, index) => const SizedBox(width: 10),
+        itemBuilder: (context, index) {
+          final isSelected = selectedIndex == index;
+          final category = widget.categories[index];
+
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              decoration: ShapeDecoration(
+                color: isSelected
+                    ? const Color(0xFF0D0D0D)
+                    : const Color(0xFFF2F2F3),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+              child: Row(
+                children: [
+                  if (category.icon != null) ...[
+                    Icon(
+                      category.icon,
+                      size: 16,
+                      color: isSelected ? Colors.white : Colors.black,
+                    ),
+                    const SizedBox(width: 4),
+                  ],
+                  Text(
+                    category.label,
+                    style: TextStyle(
+                      color: isSelected
+                          ? Colors.white
+                          : const Color(0xFF0D0D0D),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
